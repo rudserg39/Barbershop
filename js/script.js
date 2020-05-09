65939aec50f43f5ea287bba2b76d188d58e90934
Sergei Rudkovskii <rudserg39@gmail.com>
2020-05-09 19:58:38 +0200

    Add states and script

---
+++ js/script.js
@@ -0,0 +1,69 @@
+var popup = document.querySelector('.popup');
+var popupForm = popup.querySelector('.popup__form');
+
+var popupOpenBtn = document.querySelector('.login');
+var popupCloseBtn = popup.querySelector('.popup__close');
+
+var map = document.querySelector('.map');
+var mapOpenBtn = document.querySelector('.popup-buttons__route');
+var mapCloseBtn = map.querySelector('.map__close');
+
+
+function openBtnClickHandler (element, className) {
+  element.classList.add(className);
+};
+
+function closeBtnClickHandler (element, className) {
+  element.classList.remove(className);
+
+  if (element === popup) {
+    popupForm.reset();
+  }
+};
+
+/* popup */
+
+popupOpenBtn.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  openBtnClickHandler(popup, 'popup--open');
+});
+
+popupCloseBtn.addEventListener('click', function () {
+  closeBtnClickHandler(popup, 'popup--open');
+});
+
+popupCloseBtn.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 13) {
+    closeBtnClickHandler(popup, 'popup--open');
+  }
+});
+
+document.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    closeBtnClickHandler(popup, 'popup--open');
+  }
+});
+
+
+/* map */
+
+mapOpenBtn.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  openBtnClickHandler(map, 'map--open');
+});
+
+mapCloseBtn.addEventListener('click', function () {
+  closeBtnClickHandler(map, 'map--open');
+});
+
+mapCloseBtn.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 13) {
+    closeBtnClickHandler(map, 'map--open');
+  }
+});
+
+document.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    closeBtnClickHandler(map, 'map--open');
+  }
+});
